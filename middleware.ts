000bb99d49c9d7a28449bc111e0599f46c92571e
Engraya <ahmadyakubuaya@gmail.com>
2024-08-27 16:39:09 +0100

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,17 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware({
   // An array of public routes that don't require authentication.
   // @ts-ignore
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: [
+    '/',
+    "/api/webhook/clerk",
+    '/api/uploadthing'
+  ],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    '/api/uploadthing'
+  ],
 });
 
 export const config = {
